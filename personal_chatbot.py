78e95e3147d64b13df8f9461dc7b633d361c8c95
mexudi <soufianmexudi20@gmail.com>
2023-04-01 18:45:53 +0000

    add ML model for the chatbot

--- personal_chatbot.py
+++ personal_chatbot.py
@@ -37,3 +37,20 @@ intents = [
     "responses": ["Sure, what do you need help with?", "I'm here to help. What's the problem?", "How can I assist you?"]
     }
 ]
+
+# Create the vectorizer and classifier
+vectorizer = TfidfVectorizer()
+clf = LogisticRegression(random_state=0, max_iter=10000)
+
+# Preprocess the data
+tags = []
+patterns = []
+for intent in intents:
+    for pattern in intent['patterns']:
+        tags.append(intent['tag'])
+        patterns.append(pattern)
+
+# training the model
+x = vectorizer.fit_transform(patterns)
+y = tags
+clf.fit(x, y)
