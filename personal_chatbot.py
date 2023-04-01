71aff5a6f4776b3533e28a338046044d7bf7ad04
mexudi <soufianmexudi20@gmail.com>
2023-04-01 18:46:47 +0000

    add a function to chat with chatbot

--- personal_chatbot.py
+++ personal_chatbot.py
@@ -54,3 +54,12 @@ for intent in intents:
 x = vectorizer.fit_transform(patterns)
 y = tags
 clf.fit(x, y)
+
+# Lets chat with the chatbot
+def chatbot(input_text):
+    input_text = vectorizer.transform([input_text])
+    tag = clf.predict(input_text)[0]
+    for intent in intents:
+        if intent['tag'] == tag:
+            response = random.choice(intent['responses'])
+            return response
